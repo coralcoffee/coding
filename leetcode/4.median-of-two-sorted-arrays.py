d3455b05ac7786dbb634d14cee07e66a563a81db
Coral Coffee <ygchina@hotmail.com>
2023-12-10 15:42:42 -0500

    median of two array

---
+++ leetcode/4.median-of-two-sorted-arrays.py
@@ -0,0 +1,83 @@
+#
+# @lc app=leetcode id=4 lang=python3
+#
+# [4] Median of Two Sorted Arrays
+#
+# https://leetcode.com/problems/median-of-two-sorted-arrays/description/
+#
+# algorithms
+# Hard (38.34%)
+# Likes:    26683
+# Dislikes: 2937
+# Total Accepted:    2.2M
+# Total Submissions: 5.8M
+# Testcase Example:  '[1,3]\n[2]'
+#
+# Given two sorted arrays nums1 and nums2 of size m and n respectively, return
+# the median of the two sorted arrays.
+#
+# The overall run time complexity should be O(log (m+n)).
+#
+#
+# Example 1:
+#
+#
+# Input: nums1 = [1,3], nums2 = [2]
+# Output: 2.00000
+# Explanation: merged array = [1,2,3] and median is 2.
+#
+#
+# Example 2:
+#
+#
+# Input: nums1 = [1,2], nums2 = [3,4]
+# Output: 2.50000
+# Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+#
+#
+#
+# Constraints:
+#
+#
+# nums1.length == m
+# nums2.length == n
+# 0 <= m <= 1000
+# 0 <= n <= 1000
+# 1 <= m + n <= 2000
+# -10^6 <= nums1[i], nums2[i] <= 10^6
+#
+#
+#
+
+# @lc code=start
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        m, n = len(nums1), len(nums2)
+        if n < m:
+            return self.findMedianSortedArrays(nums2, nums1)
+        
+        low, high = 0, m
+
+        while low <= high:
+            partitionX = (low + high) // 2
+            partitionY = (m + n + 1) // 2 - partitionX
+
+            maxLeftX = float('-inf') if partitionX == 0 else nums1[partitionX - 1]
+            minRightX = float('inf') if partitionX == m else nums1[partitionX]
+
+            maxLeftY = float('-inf') if partitionY == 0 else nums2[partitionY - 1]
+            minRightY = float('inf') if partitionY == n else nums2[partitionY]
+
+            if maxLeftX <= minRightY and maxLeftY <= minRightX:
+                if (m + n) % 2 == 0:
+                    return (max(maxLeftX, maxLeftY) + min(minRightX, minRightY)) / 2
+                else:
+                    return max(maxLeftX, maxLeftY)
+            elif maxLeftX > minRightY:
+                high = partitionX - 1
+            else:
+                low = partitionX + 1
+        
+        return 0
+
+# @lc code=end
