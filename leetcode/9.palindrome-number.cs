d734aa069daed37fac0b842079aa5596305bd8d2
Coral Coffee <ygchina@hotmail.com>
2023-12-30 21:41:21 -0500

    IsPalindrome

---
+++ leetcode/9.palindrome-number.cs
@@ -0,0 +1,72 @@
+/*
+ * @lc app=leetcode id=9 lang=csharp
+ *
+ * [9] Palindrome Number
+ *
+ * https://leetcode.com/problems/palindrome-number/description/
+ *
+ * algorithms
+ * Easy (54.77%)
+ * Likes:    11492
+ * Dislikes: 2622
+ * Total Accepted:    3.9M
+ * Total Submissions: 7.1M
+ * Testcase Example:  '121'
+ *
+ * Given an integer x, return true if x is a palindrome, and false
+ * otherwise.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: x = 121
+ * Output: true
+ * Explanation: 121 reads as 121 from left to right and from right to left.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: x = -121
+ * Output: false
+ * Explanation: From left to right, it reads -121. From right to left, it
+ * becomes 121-. Therefore it is not a palindrome.
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: x = 10
+ * Output: false
+ * Explanation: Reads 01 from right to left. Therefore it is not a
+ * palindrome.
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * -2^31 <= x <= 2^31 - 1
+ * 
+ * 
+ * 
+ * Follow up: Could you solve it without converting the integer to a string?
+ */
+
+// @lc code=start
+public class Solution
+{
+    public bool IsPalindrome(int x)
+    {
+        string s = x.ToString();
+        int half = s.Length / 2;
+        for (int i = 0; i < half; i++)
+        {
+            if (s[i] != s[s.Length - i - 1]) return false;
+        }
+        return true;
+    }
+}
+// @lc code=end
+
