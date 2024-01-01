7efe36b1e6be71aae610f2f2d54851b372123dc0
Coral Coffee <ygchina@hotmail.com>
2023-12-31 21:13:36 -0500

    IntToRoman

---
+++ leetcode/11.container-with-most-water.cs
@@ -0,0 +1,86 @@
+/*
+ * @lc app=leetcode id=11 lang=csharp
+ *
+ * [11] Container With Most Water
+ *
+ * https://leetcode.com/problems/container-with-most-water/description/
+ *
+ * algorithms
+ * Medium (54.30%)
+ * Likes:    27256
+ * Dislikes: 1508
+ * Total Accepted:    2.5M
+ * Total Submissions: 4.6M
+ * Testcase Example:  '[1,8,6,2,5,4,8,3,7]'
+ *
+ * You are given an integer array height of length n. There are n vertical
+ * lines drawn such that the two endpoints of the i^th line are (i, 0) and (i,
+ * height[i]).
+ * 
+ * Find two lines that together with the x-axis form a container, such that the
+ * container contains the most water.
+ * 
+ * Return the maximum amount of water a container can store.
+ * 
+ * Notice that you may not slant the container.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: height = [1,8,6,2,5,4,8,3,7]
+ * Output: 49
+ * Explanation: The above vertical lines are represented by array
+ * [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the
+ * container can contain is 49.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: height = [1,1]
+ * Output: 1
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * n == height.length
+ * 2 <= n <= 10^5
+ * 0 <= height[i] <= 10^4
+ * 
+ * 
+ */
+
+// @lc code=start
+public class Solution
+{
+    public int MaxArea(int[] height)
+    {
+        int left = 0, right = height.Length - 1;
+        int maxArea = 0;
+
+        while (left < right)
+        {
+            // Calculate the area and update maxArea if necessary
+            int width = right - left;
+            int currentArea = Math.Min(height[left], height[right]) * width;
+            maxArea = Math.Max(maxArea, currentArea);
+
+            // Move the pointer pointing to the shorter line
+            if (height[left] < height[right])
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+}
+// @lc code=end
+
