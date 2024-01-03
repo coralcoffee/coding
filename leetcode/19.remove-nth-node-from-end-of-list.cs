5c6e29d0b5a4f45ffb74efd76d70bbd9ab768c21
Coral Coffee <ygchina@hotmail.com>
2024-01-03 14:58:01 -0500

    RemoveNthFromEnd

---
+++ leetcode/19.remove-nth-node-from-end-of-list.cs
@@ -0,0 +1,93 @@
+/*
+ * @lc app=leetcode id=19 lang=csharp
+ *
+ * [19] Remove Nth Node From End of List
+ *
+ * https://leetcode.com/problems/remove-nth-node-from-end-of-list/description/
+ *
+ * algorithms
+ * Medium (42.96%)
+ * Likes:    17744
+ * Dislikes: 740
+ * Total Accepted:    2.4M
+ * Total Submissions: 5.5M
+ * Testcase Example:  '[1,2,3,4,5]\n2'
+ *
+ * Given the head of a linked list, remove the n^th node from the end of the
+ * list and return its head.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: head = [1,2,3,4,5], n = 2
+ * Output: [1,2,3,5]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: head = [1], n = 1
+ * Output: []
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: head = [1,2], n = 1
+ * Output: [1]
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The number of nodes in the list is sz.
+ * 1 <= sz <= 30
+ * 0 <= Node.val <= 100
+ * 1 <= n <= sz
+ * 
+ * 
+ * 
+ * Follow up: Could you do this in one pass?
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public ListNode RemoveNthFromEnd(ListNode head, int n)
+    {
+        ListNode node = head, lastNode = head;
+
+        int index = 0;
+        while (lastNode.next != null)
+        {
+            if (index < n)
+            {
+                index++;
+            }
+            else
+            {
+                node = node.next;
+            }
+            lastNode = lastNode.next;
+        }
+        if (index < n) head = head.next;
+        else node.next = node.next.next;
+        return head;
+    }
+}
+// @lc code=end
+
