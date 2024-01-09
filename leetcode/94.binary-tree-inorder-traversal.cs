a33e8ca9120ca49bafda97a54fffb2010e7d2ab3
Coral Coffee <ygchina@hotmail.com>
2024-01-09 17:07:44 -0500

    Binary Tree Inorder Traversal

---
+++ leetcode/94.binary-tree-inorder-traversal.cs
@@ -0,0 +1,86 @@
+/*
+ * @lc app=leetcode id=94 lang=csharp
+ *
+ * [94] Binary Tree Inorder Traversal
+ *
+ * https://leetcode.com/problems/binary-tree-inorder-traversal/description/
+ *
+ * algorithms
+ * Easy (74.94%)
+ * Likes:    13032
+ * Dislikes: 730
+ * Total Accepted:    2.4M
+ * Total Submissions: 3.1M
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * Given the root of a binary tree, return the inorder traversal of its nodes'
+ * values.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: root = [1,null,2,3]
+ * Output: [1,3,2]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: root = []
+ * Output: []
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: root = [1]
+ * Output: [1]
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The number of nodes in the tree is in the range [0, 100].
+ * -100 <= Node.val <= 100
+ * 
+ * 
+ * 
+ * Follow up: Recursive solution is trivial, could you do it iteratively?
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution
+{
+    private readonly IList<int> _list = new List<int>();
+    public IList<int> InorderTraversal(TreeNode root)
+    {
+        Traversal(root);
+        return _list;
+    }
+
+    private void Traversal(TreeNode node)
+    {
+        if (node == null) return;
+
+        Traversal(node.left);
+        _list.Add(node.val);
+        Traversal(node.right);
+    }
+}
+// @lc code=end
+
