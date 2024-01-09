a5961868526f37240c7b7f13070bafa604e253cb
Coral Coffee <ygchina@hotmail.com>
2024-01-09 17:20:13 -0500

    Binary Tree Postorder Traversal

---
+++ leetcode/145.binary-tree-postorder-traversal.cs
@@ -0,0 +1,86 @@
+/*
+ * @lc app=leetcode id=145 lang=csharp
+ *
+ * [145] Binary Tree Postorder Traversal
+ *
+ * https://leetcode.com/problems/binary-tree-postorder-traversal/description/
+ *
+ * algorithms
+ * Easy (69.77%)
+ * Likes:    6663
+ * Dislikes: 183
+ * Total Accepted:    1.1M
+ * Total Submissions: 1.6M
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * Given the root of a binary tree, return the postorder traversal of its
+ * nodes' values.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: root = [1,null,2,3]
+ * Output: [3,2,1]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: root = []
+ * Output: []
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: root = [1]
+ * Output: [1]
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The number of the nodes in the tree is in the range [0, 100].
+ * -100 <= Node.val <= 100
+ * 
+ * 
+ * 
+ * Follow up: Recursive solution is trivial, could you do it iteratively?
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution
+{
+    private readonly IList<int> _list = new List<int>();
+
+    public IList<int> PostorderTraversal(TreeNode root)
+    {
+        Traversal(root);
+        return _list;
+    }
+
+    private void Traversal(TreeNode node)
+    {
+        if (node == null) return;
+        Traversal(node.left);
+        Traversal(node.right);
+        _list.Add(node.val);
+    }
+}
+// @lc code=end
+
