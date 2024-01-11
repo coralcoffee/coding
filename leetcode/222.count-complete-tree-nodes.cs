46d958405082f536de033a34839eed6d7c81b67b
Coral Coffee <ygchina@hotmail.com>
2024-01-10 20:07:31 -0500

    Count Complete Tree Nodes

---
+++ leetcode/222.count-complete-tree-nodes.cs
@@ -0,0 +1,93 @@
+/*
+ * @lc app=leetcode id=222 lang=csharp
+ *
+ * [222] Count Complete Tree Nodes
+ *
+ * https://leetcode.com/problems/count-complete-tree-nodes/description/
+ *
+ * algorithms
+ * Easy (63.14%)
+ * Likes:    8338
+ * Dislikes: 472
+ * Total Accepted:    653.3K
+ * Total Submissions: 1M
+ * Testcase Example:  '[1,2,3,4,5,6]'
+ *
+ * Given the root of a complete binary tree, return the number of the nodes in
+ * the tree.
+ * 
+ * According to Wikipedia, every level, except possibly the last, is completely
+ * filled in a complete binary tree, and all nodes in the last level are as far
+ * left as possible. It can have between 1 and 2^h nodes inclusive at the last
+ * level h.
+ * 
+ * Design an algorithm that runs in less than O(n) time complexity.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: root = [1,2,3,4,5,6]
+ * Output: 6
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: root = []
+ * Output: 0
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: root = [1]
+ * Output: 1
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The number of nodes in the tree is in the range [0, 5 * 10^4].
+ * 0 <= Node.val <= 5 * 10^4
+ * The tree is guaranteed to be complete.
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution
+{
+    private int _count = 0;
+
+    public int CountNodes(TreeNode root)
+    {
+        Count(root);
+        return _count;
+    }
+
+    private void Count(TreeNode node)
+    {
+        if (node == null) return;
+
+        Count(node.left);
+        _count++;
+        Count(node.right);
+    }
+}
+// @lc code=end
+
