36cecfe9c16a1d3902fc394a5093f770b7a69abc
Coral Coffee <ygchina@hotmail.com>
2024-01-10 21:12:06 -0500

    Range Sum Query - Immutable

---
+++ leetcode/303.range-sum-query-immutable.cs
@@ -0,0 +1,92 @@
+/*
+ * @lc app=leetcode id=303 lang=csharp
+ *
+ * [303] Range Sum Query - Immutable
+ *
+ * https://leetcode.com/problems/range-sum-query-immutable/description/
+ *
+ * algorithms
+ * Easy (61.63%)
+ * Likes:    3082
+ * Dislikes: 1882
+ * Total Accepted:    496.1K
+ * Total Submissions: 797.7K
+ * Testcase Example:  '["NumArray","sumRange","sumRange","sumRange"]\n' +
+  '[[[-2,0,3,-5,2,-1]],[0,2],[2,5],[0,5]]'
+ *
+ * Given an integer array nums, handle multiple queries of the following
+ * type:
+ * 
+ * 
+ * Calculate the sum of the elements of nums between indices left and right
+ * inclusive where left <= right.
+ * 
+ * 
+ * Implement the NumArray class:
+ * 
+ * 
+ * NumArray(int[] nums) Initializes the object with the integer array nums.
+ * int sumRange(int left, int right) Returns the sum of the elements of nums
+ * between indices left and right inclusive (i.e. nums[left] + nums[left + 1] +
+ * ... + nums[right]).
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input
+ * ["NumArray", "sumRange", "sumRange", "sumRange"]
+ * [[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
+ * Output
+ * [null, 1, -1, -3]
+ * 
+ * Explanation
+ * NumArray numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
+ * numArray.sumRange(0, 2); // return (-2) + 0 + 3 = 1
+ * numArray.sumRange(2, 5); // return 3 + (-5) + 2 + (-1) = -1
+ * numArray.sumRange(0, 5); // return (-2) + 0 + 3 + (-5) + 2 + (-1) = -3
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= nums.length <= 10^4
+ * -10^5 <= nums[i] <= 10^5
+ * 0 <= left <= right < nums.length
+ * At most 10^4 calls will be made to sumRange.
+ * 
+ * 
+ */
+
+// @lc code=start
+public class NumArray
+{
+    private int[] prefixSums;
+    public NumArray(int[] nums)
+    {
+        prefixSums = new int[nums.Length];
+        if (nums.Length == 0) return;
+
+        prefixSums[0] = nums[0];
+        for (int i = 1; i < nums.Length; i++)
+        {
+            prefixSums[i] = prefixSums[i - 1] + nums[i];
+        }
+    }
+
+    public int SumRange(int left, int right)
+    {
+        if (left == 0) return prefixSums[right];
+        return prefixSums[right] - prefixSums[left - 1];
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.SumRange(left,right);
+ */
+// @lc code=end
+
