a8949b514f985069986daedb41f6347d8a0cf74c
Coral Coffee <ygchina@hotmail.com>
2024-02-09 16:56:06 -0500

    2 java

---
+++ leetcode/2.add-two-numbers.java
@@ -0,0 +1,92 @@
+/*
+ * @lc app=leetcode id=2 lang=java
+ *
+ * [2] Add Two Numbers
+ *
+ * https://leetcode.com/problems/add-two-numbers/description/
+ *
+ * algorithms
+ * Medium (41.52%)
+ * Likes:    28995
+ * Dislikes: 5622
+ * Total Accepted:    4.1M
+ * Total Submissions: 9.8M
+ * Testcase Example:  '[2,4,3]\n[5,6,4]'
+ *
+ * You are given two non-empty linked lists representing two non-negative
+ * integers. The digits are stored in reverse order, and each of their nodes
+ * contains a single digit. Add the two numbers and return the sum as a linked
+ * list.
+ * 
+ * You may assume the two numbers do not contain any leading zero, except the
+ * number 0 itself.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: l1 = [2,4,3], l2 = [5,6,4]
+ * Output: [7,0,8]
+ * Explanation: 342 + 465 = 807.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: l1 = [0], l2 = [0]
+ * Output: [0]
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+ * Output: [8,9,9,9,0,0,0,1]
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The number of nodes in each linked list is in the range [1, 100].
+ * 0 <= Node.val <= 9
+ * It is guaranteed that the list represents a number that does not have
+ * leading zeros.
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode();
+        ListNode current = head;
+        int sum = 0;
+        int carrier = 0;
+        while (l1 != null || l2 != null) {
+            int x = l1 == null ? 0 : l1.val;
+            int y = l2 == null ? 0 : l2.val;
+            sum = x + y + carrier;
+            current.next = new ListNode(sum % 10);
+            current = current.next;
+            carrier = sum / 10;
+            l1 = l1 != null ? l1.next : null;
+            l2 = l2 != null ? l2.next : null;
+        }
+
+        if (carrier>0) current.next = new ListNode(carrier);
+        
+        return head.next;
+    }
+}
+// @lc code=end
