db9167a7dacffc6e896b0c33069f20eebaa7695c
Coral Coffee <ygchina@hotmail.com>
2024-10-16 14:35:56 -0400

    3 Java

--- leetcode/3.longest-substring-without-repeating-characters.java
+++ leetcode/3.longest-substring-without-repeating-characters.java
@@ -54,16 +54,42 @@
  */
 
 // @lc code=start
+
+import java.util.HashMap;
+
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        Map<Character, Integer> dict = new HashMap<>();
-        int start = -1;
         int result = 0;
+        HashMap<Character, Integer> dict = new HashMap<>();
+        int start = -1;
         for (int i = 0; i < s.length(); i++) {
-            if (dict.containsKey(s.charAt(i))) // Fixed the indexing issue
-                start = Math.max(dict.get(s.charAt(i)), start); // Fixed the indexing issue
-            result = Math.max(i - start, result);
-            dict.put(s.charAt(i), i); // Fixed the indexing issue
+            char c = s.charAt(i);
+            if (dict.containsKey(c)) {
+                start = Math.max(dict.get(c), start);
+            }
+            dict.put(c, i);
+
+            result = Math.max(result, i - start);
+        }
+        return result;
+    }
+
+    // faster for small set of test data
+    public int lengthOfLongestSubstring1(String s) {
+        if (s == null || s.isEmpty())
+            return 0;
+        int result = 1;
+        int slow = 0, fast = 0;
+        for (int i = 1; i < s.length(); i++) {
+            char c = s.charAt(i);
+            for (int j = slow; j <= fast; j++) {
+                if (s.charAt(j) == c) {
+                    slow = j + 1;
+                    break;
+                }
+            }
+            fast = i;
+            result = Math.max(result, fast - slow + 1);
         }
         return result;
     }
