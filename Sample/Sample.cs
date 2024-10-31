eb0c8be106081f288c7b3dc972682d33262f1bab
Coral Coffee <ygchina@hotmail.com>
2024-10-31 10:11:36 -0400

    Sample

---
+++ Sample/Sample.cs
@@ -0,0 +1,18 @@
+public class Sample
+{
+    int BinarySearch(int[] nums, int target)
+    {
+        int i = 0, j = nums.Length - 1;
+        while (i <= j)
+        {
+            int m = i + (j - i) / 2;
+            if (nums[m] < target)
+                i = m + 1;
+            else if (nums[m] > target)
+                j = m - 1;
+            else
+                return m;
+        }
+        return -1;
+    }
+}
