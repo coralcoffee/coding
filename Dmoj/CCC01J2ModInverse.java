2c84863c5fe20cd2118e7fdd7fc5f831a8c01c88
Coral Coffee <ygchina@hotmail.com>
2024-10-31 10:40:23 -0400

    CCC01J2ModInverse

---
+++ Dmoj/CCC01J2ModInverse.java
@@ -0,0 +1,26 @@
+package Dmoj;
+
+import java.util.*;
+
+// CCC '01 J2 - Mod Inverse
+public class CCC01J2ModInverse {
+    public static void main(String[] args) {
+        String input = """
+                4
+                133
+                            """;
+        Scanner inp = new Scanner(input);
+        int x = Integer.parseInt(inp.nextLine());
+        int m = Integer.parseInt(inp.nextLine());
+        inp.close();
+        int n = m / x;
+        while (n < m && x * n % m != 1) {
+            n++;
+        }
+        if (n < m) {
+            System.out.println(n);
+        } else {
+            System.out.println("No such integer exists.");
+        }
+    }
+}
