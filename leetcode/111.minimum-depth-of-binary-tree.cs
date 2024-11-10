3cf5cb4aeacaa4bf55db4547639f92955547ecd9
Coral Coffee <ygchina@hotmail.com>
2024-11-10 10:37:26 -0500

    111

---
+++ leetcode/111.minimum-depth-of-binary-tree.cs
@@ -0,0 +1,98 @@
+/*
+ * @lc app=leetcode id=111 lang=csharp
+ *
+ * [111] Minimum Depth of Binary Tree
+ *
+ * https://leetcode.com/problems/minimum-depth-of-binary-tree/description/
+ *
+ * algorithms
+ * Easy (46.84%)
+ * Likes:    7377
+ * Dislikes: 1319
+ * Total Accepted:    1.3M
+ * Total Submissions: 2.7M
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * Given a binary tree, find its minimum depth.
+ * 
+ * The minimum depth is the number of nodes along the shortest path from the
+ * root node down to the nearest leaf node.
+ * 
+ * Note: A leaf is a node with no children.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: root = [3,9,20,null,null,15,7]
+ * Output: 2
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: root = [2,null,3,null,4,null,5,null,6]
+ * Output: 5
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The number of nodes in the tree is in the range [0, 10^5].
+ * -1000 <= Node.val <= 1000
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public int MinDepth(TreeNode root)
+    {
+        if (root == null) return 0;
+
+        Queue<TreeNode> queue = new Queue<TreeNode>();
+        queue.Enqueue(root);
+
+        int depth = 1;
+        while (queue.Count > 0)
+        {
+            int count = queue.Count;
+            for (int i = 0; i < count; i++)
+            {
+                var node = queue.Dequeue();
+                if (node.left == null && node.right == null)
+                {
+                    return depth;
+                }
+
+                if (node.left != null)
+                {
+                    queue.Enqueue(node.left);
+                }
+                if (node.right != null)
+                {
+                    queue.Enqueue(node.right);
+                }
+            }
+            depth++;
+        }
+        return depth;
+    }
+}
+// @lc code=end
+
